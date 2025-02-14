a019b35e80730929f85c6a102d7c1b26be7c0941
Myeongjun Kim <audwnssp@naver.com>
2025-02-15 02:04:42 +0900

    init spring

---
+++ src/main/java/com/cmyk/ego/speaktoyouspring/SpeakToYouSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cmyk.ego.speaktoyouspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakToYouSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakToYouSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cmyk/ego/speaktoyouspring/SpeakToYouSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmyk.ego.speaktoyouspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakToYouSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
