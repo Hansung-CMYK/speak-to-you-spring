f7b901bd41c63d45817fa7c0eaf7d0ed87418df9
junni01kim <audwnssp@naver.com>
2025-02-15 14:48:12 +0900

    [error commit] basic infra

---
+++ src/main/java/com/cmyk/ego/speaktoyouspring/common/CommonResponse.java
@@ -0,0 +1,14 @@
+package com.cmyk.ego.speaktoyouspring.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CommonResponse<T> {
+    private int code;
+    private String message;
+    private T data;
+}
