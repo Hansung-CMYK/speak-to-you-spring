608fafb42df1c9a59550550a13c884bf8785bedd
KJH0506 <pete05006017@gmail.com>
2025-04-07 13:57:45 +0900

    [feat/get-conversation-list] feat : ConversationLog의 전체 데이터 조회
    - SchemaName에 따른 전체 Topic반환

--- src/main/java/com/cmyk/ego/speaktoyouspring/api/tenant/conversationlog/ConversationLogController.java
+++ src/main/java/com/cmyk/ego/speaktoyouspring/api/tenant/conversationlog/ConversationLogController.java
@@ -1,8 +1,10 @@
 package com.cmyk.ego.speaktoyouspring.api.tenant.conversationlog;
 
+import com.cmyk.ego.speaktoyouspring.api.hub.tenant.TenantService;
 import com.cmyk.ego.speaktoyouspring.config.CommonResponse;
 import com.cmyk.ego.speaktoyouspring.config.multitenancy.TenantContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,7 +12,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/conversationLog/")
 @RequiredArgsConstructor
 public class ConversationLogController {
+
+    @Autowired
     private final ConversationLogService conversationLogService;
+
+    @Autowired
+    private final TenantService tenantService;
 
     // Test 생성
     @PostMapping("create/{schemaName}")
@@ -19,5 +26,33 @@ public class ConversationLogController {
         var result = conversationLogService.create(conversationLog);
 
         return ResponseEntity.ok(CommonResponse.builder().code(200).message("대화 내역 생성 완료").data(result).build());
+    }
+
+    // conversation_log에 기록된 전체 topic반환
+    @GetMapping("{schemaName}/conversation-logs")
+    public ResponseEntity<?> readAll(@PathVariable String schemaName) {
+        try {
+            TenantContext.setCurrentTenant(schemaName);
+            var result = conversationLogService.readAll();
+
+            return ResponseEntity.ok(
+                    CommonResponse.builder()
+                            .code(200)
+                            .message("전체 주제 조회")
+                            .data(result)
+                            .build()
+            );
+        } catch (Exception e) { // Schema가 없는 경우
+            e.printStackTrace();
+            return ResponseEntity.status(404).body(
+                    CommonResponse.builder()
+                            .code(404)
+                            .message("존재하지 않는 테넌트(스키마)입니다.")
+                            .data(null)
+                            .build()
+            );
+        } finally {
+            TenantContext.clear();
+        }
     }
 }
--- src/main/java/com/cmyk/ego/speaktoyouspring/api/tenant/conversationlog/ConversationLogService.java
+++ src/main/java/com/cmyk/ego/speaktoyouspring/api/tenant/conversationlog/ConversationLogService.java
@@ -3,6 +3,8 @@ package com.cmyk.ego.speaktoyouspring.api.tenant.conversationlog;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ConversationLogService {
@@ -15,5 +17,9 @@ public class ConversationLogService {
                         .topic(conversationLog.getTopic())
                         .build()
         );
+    }
+
+    public List<ConversationLog> readAll() {
+        return conversationLogRepository.findAll();
     }
 }
