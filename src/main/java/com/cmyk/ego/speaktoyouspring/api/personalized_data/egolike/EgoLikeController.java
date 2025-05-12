cc9abe12bd91b92511df5c9f2b123483378e18a5
HS-JNYLee <2071013@hansung.ac.kr>
2025-05-13 07:22:08 +0900

    [CMYK-181] feat: Ego 하트 토글 API
    - /api/v1/ego-like/ POST
    - EGO_LIKE 테이블 생성

--- src/main/java/com/cmyk/ego/speaktoyouspring/api/personalized_data/egolike/EgoLikeController.java
+++ src/main/java/com/cmyk/ego/speaktoyouspring/api/personalized_data/egolike/EgoLikeController.java
@@ -3,11 +3,13 @@ package com.cmyk.ego.speaktoyouspring.api.personalized_data.egolike;
 import com.cmyk.ego.speaktoyouspring.api.hub.ego.Ego;
 import com.cmyk.ego.speaktoyouspring.api.hub.ego.EgoRepository;
 import com.cmyk.ego.speaktoyouspring.api.hub.user_account.UserAccountRepository;
+import com.cmyk.ego.speaktoyouspring.config.CommonResponse;
 import com.cmyk.ego.speaktoyouspring.config.multitenancy.TenantContext;
 import com.cmyk.ego.speaktoyouspring.exception.ControlledException;
 import com.cmyk.ego.speaktoyouspring.exception.errorcode.EgoErrorCode;
 import com.cmyk.ego.speaktoyouspring.exception.errorcode.UserAccountErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,7 +24,7 @@ public class EgoLikeController {
      * ego 좋아요 등록
      */
     @PostMapping("/")
-    public Ego update(@RequestBody EgoLikeDTO egoLikeDTO) {
+    public ResponseEntity update(@RequestBody EgoLikeDTO egoLikeDTO) {
 
         // 전달받은 Uid가 있는지 확인
         userAccountRepository.findByUid(egoLikeDTO.getUid()).orElseThrow(() -> new ControlledException(UserAccountErrorCode.ERROR_USER_NOT_FOUND));
@@ -31,6 +33,6 @@ public class EgoLikeController {
 
         TenantContext.setCurrentTenant(egoLikeDTO.getUid());
 
-        return egoLikeService.updateLikes(egoLikeDTO);
+        return ResponseEntity.ok(CommonResponse.builder().code(200).message("Update: 좋아요 업데이트된 ego 조회 완료").data(egoLikeService.updateLikes(egoLikeDTO)).build());
     }
 }
